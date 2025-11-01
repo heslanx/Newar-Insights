85e372ef09afc0e324a99a49d3643cc2c2541982
heslanx <167841364+heslanx@users.noreply.github.com>
2025-11-01 14:33:22 -0300

    Delete examples/Vexa-Clean directory

--- examples/Vexa-Clean/bot/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export { log, randomDelay, callStartupCallback, callJoiningCallback, callAwaitingAdmissionCallback, callLeaveCallback } from '../utils';
-export { WebSocketManager, type WebSocketConfig, type WebSocketEventHandlers } from './websocket';
-export { 
-  BrowserAudioService, 
-  BrowserWhisperLiveService, 
-  generateBrowserUUID 
-} from './browser';
